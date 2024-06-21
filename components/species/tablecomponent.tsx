22d9ece850031c9f353433c0f113c493c4961175
etbayes <etbayes@gmail.com>
2024-06-21 15:58:53 -0400

    change path to tasks

--- components/species/tablecomponent.tsx
+++ components/species/tablecomponent.tsx
@@ -1,33 +1,19 @@
-import { promises as fs } from "fs"
-import path from "path"
 import { Metadata } from "next"
-import Image from "next/image"
 import { z } from "zod"
 
 import { columns } from "./components/columns"
 import { DataTable } from "./components/data-table"
-import { UserNav } from "./components/user-nav"
 import { taskSchema } from "./data/schema"
+import tasksData from "./data/tasks.json"
 
 export const metadata: Metadata = {
   title: "Tasks",
   description: "A task and issue tracker build using Tanstack Table.",
 }
 
-// Simulate a database read for tasks.
-async function getTasks() {
-  const data = await fs.readFile(
-    path.join("./components/species/data/tasks.json")
-  )
+const tasks = z.array(taskSchema).parse(tasksData)
 
-  const tasks = JSON.parse(data.toString())
-
-  return z.array(taskSchema).parse(tasks)
-}
-
-export default async function TaskPage() {
-  const tasks = await getTasks()
-
+export default function TaskPage() {
   return (
     <>
         <DataTable data={tasks} columns={columns} />
