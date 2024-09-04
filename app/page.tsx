afe69a799905c4f01ad0c21e879c12d54833d003
Chris Manciero <cmanciero@gmail.com>
2024-09-04 13:21:20 -0400

    sample hello world

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Main from "@/components/main/page";
+import Main from '@/components/main/page';
 
 export default function Page() {
   return (
     <>    
       <div className="flex w-screen h-screen">
+        <h1>Hello world</h1>
         <Main/>
       </div>
     </>
